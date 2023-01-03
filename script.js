3db4bea3d787b8fb9a1f5a907243451440ad6c16
Adam McSweeney <adammcsweeney@gmail.com>
2023-01-03 04:55:08 +0000

    Completed gameplay.

--- script.js
+++ script.js
@@ -14,23 +14,49 @@ function getComputerChoice(){
 }  
 
 function playRound(playerSelection, computerSelection){
-    if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        return "Win"
-    } else if (playerSelection === "Rock" && computerSelection === "Paper"){
-        return "Lose"
-    } else if (playerSelection === "Paper" && computerSelection === "Rock"){
-        return "Win"
-    } else if (playerSelection === "Paper" && computerSelection === "Scissors"){
-        return "Lose"
-    } else if (playerSelection === "Scissors" && computerSelection === "Rock"){
-        return "Lose"
-    } else if (playerSelection === "Scissors" && computerSelection === "Paper"){
-        return "Win"
+    if (playerSelection === "ROCK" && computerSelection === "SCISSORS"){
+        return "You Win! Rock beats Scissors!"
+    } else if (playerSelection === "ROCK" && computerSelection === "PAPER"){
+        return "You Lose! Paper beats Rock!"
+    } else if (playerSelection === "PAPER" && computerSelection === "ROCK"){
+        return "You Win! Paper beats Rock!"
+    } else if (playerSelection === "PAPER" && computerSelection === "SCISSORS"){
+        return "You Lose! Scissors beat Paper!"
+    } else if (playerSelection === "SCISSORS" && computerSelection === "ROCK"){
+        return "You Lose! Rock beats Scissors!"
+    } else if (playerSelection === "SCISSORS" && computerSelection === "PAPER"){
+        return "You Win! Scissors beats Paper"
+    } else if (playerSelection === computerSelection){
+        return "It is a Draw!"
     } else {
-        return "Draw"
-    }}
+        return 
+    }
+}
 
-let playerSelection = getComputerChoice()
-let computerSelection = getComputerChoice()
-console.log("You: " + playerSelection + " CPU: " + computerSelection)
-console.log(playRound(playerSelection, computerSelection))
+function game(){
+    let playerScore = 0
+    let cpuScore = 0
+    for (i = 0; i < 5; i++){      
+        let playerSelection = prompt("Rock, Paper, Scissors?").toUpperCase()
+        let computerSelection = getComputerChoice().toUpperCase()
+        
+        let outCome = playRound(playerSelection, computerSelection)
+
+        if (outCome.charAt(4) === "W"){
+            playerScore++
+        } else if (outCome.charAt(4) === "L"){
+            cpuScore++
+        }
+        console.log("Round: " + (i + 1))
+        console.log("Player: " + playerSelection + " Score: " + playerScore)
+        console.log("Computer: " + computerSelection + " Score: " + cpuScore)
+        console.log(outCome)
+    }
+    if (playerScore > cpuScore){
+        console.log("You Won The Game!")
+    } else {
+        console.log("You Lost The Game!")
+    }
+}
+
+game()
