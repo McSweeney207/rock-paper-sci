324ddd876797ab3555a8f2ff9533a5f808bfd894
Adam McSweeney <adammcsweeney@gmail.com>
2023-01-15 05:23:00 +0000

    Complete basic GUI, cleaned up code and added comments.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # rock-paper-sci
+
+Rock Paper Scissors project for the Odin Project.
+
+Basic Gui, planning to create a better GUI with some custom images.
--- script.js
+++ script.js
@@ -1,18 +1,19 @@
+//HTML Query Selectors
 const buttons = document.querySelectorAll('button');
 const result = document.querySelector('.result')
 const playerInput = document.getElementById('player-input')
 const computerInput = document.getElementById('computer-input')
 const playerScoreOutput = document.getElementById('player-score')
 const computerScoreOutput = document.getElementById('computer-score')
-const roundDisplay = document.getElementById('game-round')
 const gamePlay = document.querySelector('.game-play')
 const endGame = document.querySelector('.end-game')
 const endResult = document.getElementById('end-result')
 
+//Global Varibles
 let playerScore = 0
 let cpuScore = 0
-let gameRound = 1
 
+//Randomly selects Rock Paper or Scissors.
 function getComputerChoice(){
     let choice = Math.trunc(Math.random()*3) + 1;
     switch (choice){
@@ -28,6 +29,7 @@ function getComputerChoice(){
     }
 }  
 
+//Compares players selection with Random computer Choice.
 function playRound(playerSelection, computerSelection){
     if (playerSelection === "Rock" && computerSelection === "Scissors"){
         return "You Win! Rock beats Scissors!"
@@ -48,18 +50,18 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
+//Checks to see who wins and updates the scores accordingly.
 function updateScores(game){
     if (game.charAt(4) === "W"){
         playerScore++
     } else if (game.charAt(4) === "L"){
         cpuScore++
     }
-    gameRound++
-
     playerScoreOutput.textContent = playerScore
     computerScoreOutput.textContent = cpuScore
 }
 
+//Checks to see if either player has reached 5 points and then Switches to the end of game HTML Divs.
 function endOfGame(){
     if (playerScore === 5){
         gamePlay.style.display = "none"
@@ -74,18 +76,17 @@ function endOfGame(){
     }
 }
 
-function gameReset(){   
+//Resets game back to defult values.
+function gameReset(){
+    //Hides End game message and displays main game Div.   
+    gamePlay.style.display = "block"
+    endGame.style.display = "none"
+    
+    //Resets values and pushed them to DOM.
     playerScore = 0
     cpuScore = 0
-    gameRound = 1
-
-    gamePlay.style.display = "block"
-    endGame.style.display = "none"  
-
     playerScoreOutput.textContent = playerScore
-    computerScoreOutput.textContent = cpuScore
-
-    
+    computerScoreOutput.textContent = cpuScore 
     playerInput.textContent = ''
     computerInput.textContent = ''
     result.textContent = ''
@@ -94,18 +95,21 @@ function gameReset(){
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
     
-    if (button.id === 'playAgain'){
-        gameReset()
+    if (button.id === 'play-again'){
+        gameReset() //Resets game back to defult values.
     } else {
+
+        //Gets players and computers Choice and Updates Text in divs
         computer = getComputerChoice()    
         game = playRound(button.id, computer)
     
+        //Displays players and computers Choice, as well as the outcome.
         playerInput.textContent = button.id
         computerInput.textContent = computer
         result.textContent = game
-        updateScores(game)
-        endOfGame()
+        
+        updateScores(game) //Checks to see who wins and updates the scores accordingly.
+        endOfGame() //Checks to see if either player has reached 5 points and then Switches to the end of game HTML Divs.
     }
     });
   });
-
